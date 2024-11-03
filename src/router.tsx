1a79b8b17c8b9c964e108383923071b7252c51fa
Juan Javier Geringer <juanjaviergeringer@gmail.com>
2024-11-03 18:16:27 -0300

    optimizando la ruta index

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { lazy, Suspense } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import IndexPage from "./views/IndexPage"
 import Layout from "./layouts/Layout"
+const IndexPage = lazy(()=> import("./views/IndexPage"))
 const FavoritesPage = lazy(()=> import("./views/FavoritesPage"))
 
 const AppRouter = () => {
@@ -9,7 +9,11 @@ const AppRouter = () => {
     <BrowserRouter>
         <Routes>
             <Route element={<Layout/>}>
-              <Route path='/' element={<IndexPage/>} index/>
+              <Route path='/' element={
+                <Suspense fallback='Cargando...'>
+                <IndexPage/>
+              </Suspense>
+              } index/>
               <Route path='/favoritos' element={
                 <Suspense fallback='Cargando...'>
                   <FavoritesPage/>
